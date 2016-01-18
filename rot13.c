83699f43d904712887f66ce667436c4245cb8ee0
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-01-18 21:00:17 -0200

    Update rot13.c
    Só letras minusculas

--- rot13.c
+++ rot13.c
@@ -3,22 +3,31 @@
 #include <string.h>
 
 int opt = 0;
-int x = 0;
-char alfabeto_lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-//char alfabeto_upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int x,y = 0;
+char alfabeto[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char entrada [];
 
 relacao(){
-    for (x = 0; x < 13; x++){
-        printf("%c <-> %c\n",alfabeto[x],alfabeto[x+12%26]);
+    for (x = 0; x < strlen(alfabeto)/2; x++){
+        printf("%c <-> %c\n",alfabeto[x],alfabeto[x+13%26]);
     }
     menu();
 }
 
 cripto(int chave){
     printf("Digite a frase: \n");
-    getstr("")
+    scanf("%s", &entrada);
 
-}
+    for (x = 0; x < strlen(entrada); x++){
+
+      for(y = 0; y < strlen(alfabeto); y++){
+          if(entrada[x] == alfabeto[y]){
+            printf("%c",alfabeto[(y+13)%26]);
+          }
+        }
+        y = 0;
+      }
+    }
 
 menu(){
     printf("Digite a opção que deseja:\n\n\
@@ -28,7 +37,7 @@ menu(){
     \n\
     :");
     scanf("%d", &opt);
-    
+
     if (opt == 3){
         relacao();
     }
